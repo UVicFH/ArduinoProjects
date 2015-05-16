b29697b74687cf40771271ab817c25b6eb22fbdc
marclave <laventure.marc@gmail.com>
2015-05-15 18:47:08 -0700

    Final code push

--- WaterFlowSensor/WaterFlowSensor.ino
+++ WaterFlowSensor/WaterFlowSensor.ino
@@ -2,11 +2,11 @@
 * Author: Marc Laventure
 * Date: May 8th 2015
 * Description: Water Flow Sensor (FS400A G1) readings from digital IO to compute the Mass Flow Rate
-* Sensor Reference Manual: http://ultisensor.com/post/G1-Hall-Effect-Flow-Sensor-USN-HS10TA-1-60Lmin.html
 *
 */
-#define WATER_FLOW_SENSOR_PIN 1
+#define WATER_FLOW_SENSOR_PIN 2 
 volatile uint32_t rotorFrequency;
+int count = 1;
 
 /**
 * The Interrupt Service Routine (ISR) for incrememnting the pulse to achieve frequency
@@ -16,26 +16,32 @@ void rotorPulseInterrupt(void)
   rotorFrequency++;
 }
 
-void setup(void) 
+void setup(void)
 {
   pinMode(WATER_FLOW_SENSOR_PIN, INPUT); // Set digital pin 2 as sensors input
   attachInterrupt(0, rotorPulseInterrupt, RISING); // Setup the interrupt for rising edge detection
-  Serial.begin(9600); 
+  Serial.begin(9600);
 }
 
-void loop(void) 
+void loop(void)
 {
   float massFlowRate;
   rotorFrequency = 0;
-  
+
   sei(); // Enable interrupts for retrieving data
   delay(1000); // Wait for 1 second (simplifies calculation)
   cli(); // Disable interrupts because we dont want any more after we retrieved our data
-  
+
   /* Had to do research on this, but the reference manual for this sensor states the
-    calculation formula is Frequency = 4.6*Q(Units of Flow)*t(time in seconds), where Q is the Mass Flow Rate
+    calculation formula is Frequency = 4.8*Q(Units of Flow)*t(time in seconds)/.99910(Conversion factor of litres of water to kg), where Q is the Mass Flow Rate
   */
-  massFlowRate = ((rotorFrequency/4.6)*60); // Gives us Flow Rate in Litres per minute
-  Serial.print(massFlowRate);
-  Serial.print(" Litres/minute");
+  massFlowRate = (float) ((rotorFrequency*.99910)/(60*4.8)); // Gives us Flow Rate in kg per second
+  Serial.print(massFlowRate, 4);
+  Serial.println(" kg/second");
+  
+  count++;
+  if(count%60 == 0)
+  {
+    Serial.println("Done 60 second interval");
+  }
 }
